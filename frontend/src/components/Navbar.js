5b637a9ed58a0513da30cec6b36c5f5787f09f5d
Prakhar Gupta <prakhar897@gmail.com>
2023-12-05 16:40:05 +0530

    x

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { connect } from "react-redux";
+
+/*
+	button animation not happen on trackpad.
+*/
+const Navbar = () => {};
+
+export default connect(null, null)(Navbar);
