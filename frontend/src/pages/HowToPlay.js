b9065c2ca35e345f315cd6c1ff491c0f85e36470
Prakhar Gupta <prakhar897@gmail.com>
2023-12-30 02:50:00 +0530

    x

--- frontend/src/pages/HowToPlay.js
+++ frontend/src/pages/HowToPlay.js
@@ -3,6 +3,8 @@ import Rules from "../components/Rules";
 
 import { connect } from "react-redux";
 
+import looping_gif from "../assets/looping_gif.gif";
+
 import Navbar from "../components/Navbar";
 const HowToPlay = () => {
 	return (
@@ -15,6 +17,11 @@ const HowToPlay = () => {
 
 			<div className="col-span-6 bg-gray-400 h-screen w-full">
 				<div className="grid grid-rows-10 w-full h-max bg-gray-400">
+					<img
+						src={looping_gif} // Replace with your GIF file path
+						alt="Looping GIF"
+						className="w-auto h-auto" // Add appropriate styling
+					/>
 					<Rules />
 				</div>
 			</div>
